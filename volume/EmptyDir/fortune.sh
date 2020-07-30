7083addf9d5d9457e04b03694a3fd77aae7a8e6e
TAEKnical <ilikebigmac@naver.com>
2020-07-30 05:41:33 +0000

    0728~0730

--- volume/EmptyDir/fortune.sh
+++ volume/EmptyDir/fortune.sh
@@ -3,7 +3,7 @@ trap "exit" SIGINT
 mkdir /var/htdocs
 while :
 do
-  echo $(date) Writing fortune to /var/htdocs/index.html
-  /usr/games/fortune > /var/htdocs/index.html
-  sleep 3
+    echo $(date) Writing fortune to /var/htdocs/index.html
+    /usr/games/fortune > /var/htdocs/index.html
+    sleep 3
 done
